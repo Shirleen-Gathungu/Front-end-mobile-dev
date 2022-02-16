add3631eb40dfd1f289d16fd17da18cb1f578393
Shirleen19 <muthonishirleen2002@gmail.com>
2022-02-16 20:11:04 +0300

    data-types-assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+        studentRecords(name="Jacinta Murimbi",age=23,weight=69.7,phoneNumber=254754321654,kenyan=true)
+        studentRecords(name="Peter Osingo Adichie",age=19,weight=77.0,phoneNumber=25675678909,kenyan=false)
+        studentRecords(name="Loraine Mawingu",age=20,weight=54.6,phoneNumber=254768908732,kenyan=true)
+        studentRecords(name="Oscar Abdul Juma",age=24,weight=44.9,phoneNumber=2507323216547,kenyan=false)
+        studentRecords(name="Valeria Nzilani BIter9",age=18,weight=50.2,phoneNumber=2547113716549,kenyan=true)
+        studentRecords(name="Jacob Mwangi Wainaina",age=23,weight=63.5,phoneNumber=254786321424,kenyan=true)
+        studentRecords(name="Esther Malandi",age=20,weight=45.3,phoneNumber=2517682367901,kenyan=false)
+        studentRecords(name="Victor Philips Otiende",age=22,weight=73.0,phoneNumber=25475807632,kenyan=true)
+        studentRecords(name="Melinda Orengo",age=21,weight=60.0,phoneNumber=254711657092,kenyan=true)
+        studentRecords(name="Malachi Adichie Ngozi",age=20,weight=48.9,phoneNumber=234765742347,kenyan=false)
+        studentRecords(name="Dolcie Mary Natete",age=18,weight=64.3,phoneNumber=2547431289060,kenyan=true)
+        studentRecords(name="Dulgson Wendo Juma",age=23,weight=70.1,phoneNumber=25473456789012,kenyan=true)
+        studentRecords(name="Jane Priscah Muthoni",age=19,weight=57.2,phoneNumber=254719876543,kenyan=true)
+        studentRecords(name="Mark Dawson",age=24,weight=63.1,phoneNumber=250712432567,kenyan=false)
+        studentRecords(name="Esther Malandi",age=20,weight=45.3,phoneNumber=25675644321,kenyan=false)
+        studentRecords(name="Viola Omina",age=18,weight=46.4,phoneNumber=25471345679,kenyan=true)
+        studentRecords(name="Martin Ojode",age=20,weight=69.5,phoneNumber=254736808000,kenyan=true)
+        studentRecords(name="Shamila Mohamud",age=21,weight=50.1,phoneNumber=251706870099,kenyan=false)
+        studentRecords(name="Melania Dawkson",age=19,weight=60.5,phoneNumber=250768907000,kenyan=false)
+        studentRecords(name="Jamil Morangiri",age=24,weight=73.2,phoneNumber=254768900342, kenyan=true)
+        studentRecords(name="Beldine Siakari",age=20,weight=67.8,phoneNumber=254722111433,kenyan=true)
+    }
+    fun studentRecords(name: String,age:Int,weight: Double,phoneNumber: Long,kenyan: Boolean){
+        println(name)
+        println(age)
+        println(weight)
+        println(phoneNumber)
+        println(kenyan)
+    }
+
+
